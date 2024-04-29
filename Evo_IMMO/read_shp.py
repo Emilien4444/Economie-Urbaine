7cf5fb98b2f5f2bcf3fe52be03e36cde2104ff87
naelbe <128997008+naelbe@users.noreply.github.com>
2024-04-29 20:01:04 +0200

    Delete Evo_IMMO/read_shp.py

--- Evo_IMMO/read_shp.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-import matplotlib.pyplot as plt
-
-# Read the shapefile into a GeoDataFrame
-gdf = gpd.read_file("sh_communes.shp")
-
-# Plot the GeoDataFrame
-gdf.plot()
-plt.show()
