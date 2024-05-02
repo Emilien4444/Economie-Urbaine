283329f46e7f2e1a26d0e5681c09cfcf603736a2
Ewen001 <131710430+Ewen001@users.noreply.github.com>
2024-05-02 22:55:51 +0200

    Add files via upload

---
+++ Evo_IMMO/immo_arr.py
@@ -0,0 +1,43 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import matplotlib.colors
+import matplotlib.cm as cm
+import numpy as np
+
+file = r"C:\Users\ewena\Documents\Ecole\Economie\macro\projet\dv3f_prix_volumes_communes_2020_2022.xlsx"  
+geo = r"C:\Users\ewena\Documents\Ecole\Economie\macro\projet\ARRONDISSEMENT_MUNICIPAL.shp" 
+
+df_pi_m = pd.read_excel(file, sheet_name="Ensemble des maisons")
+#print(df_pi_m['pxm2_median_cod111'])
+df_pi_a = pd.read_excel(file, sheet_name="Ensemble des appartements") #, header = [0, 1] ? 
+#print(df_pi_a['pxm2_median_cod121'])
+df_pi = df_pi_m.merge(df_pi_a, left_on="codgeo", right_on="codgeo") #pb merge
+#print(df_pi.head(5))
+
+cdt_1 = np.isnan(df_pi["pxm2_median_cod111"])
+cdt_2 = np.isnan(df_pi["pxm2_median_cod121"])
+df_pi["pxm2_median_all"] = np.where(cdt_1, np.where(cdt_2, np.nan,df_pi["pxm2_median_cod121"]), np.where(cdt_2,df_pi["pxm2_median_cod111"], (df_pi["pxm2_median_cod121"]+df_pi["pxm2_median_cod111"])/2))
+#print(df_pi['pxm2_median_all'])
+
+geo_df = gpd.read_file(geo) #g #avec tout fichiers ds un meme dossier
+
+
+df_f = geo_df.merge(df_pi, left_on="INSEE_ARM", right_on="codgeo")
+
+nan_count = df_pi["pxm2_median_all"].isna().sum()
+
+nan_count
+
+fig, ax = plt.subplots(1, 1, figsize=(30, 15))
+cmap = cm.viridis
+
+vmin = df_f["pxm2_median_all"].min()
+vmax = df_f["pxm2_median_all"].max()
+
+com_map = df_f.plot(column="pxm2_median_all", ax=ax, legend=True, legend_kwds={'label': "prix median par m2 à Lyon", 'orientation':"horizontal"}, linewidth=1, alpha=0.9, edgecolor="black", cmap=cmap, vmin=vmin, vmax=vmax)
+
+
+plt.title("carte des prix médians de la moyenne entre prix par m2 pour appartement et maisons par arrondissements de Lyon.")
+plt.show()
